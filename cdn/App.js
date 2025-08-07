3e40e7efbd3786cf729949146181a9f62b429ed3
nillmishra <nillmishra09@gmail.com>
2025-08-08 00:22:53 +0530

    updated

--- cdn/App.js
+++ cdn/App.js
@@ -1,4 +1,4 @@
 const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 console.log(heading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); //render jo hai uss object ko lender h1 tag banakar div mai daal raha hai
+root.render(heading); //render jo hai uss object ko lender h1 tag banakar div mai daal raha hai 
